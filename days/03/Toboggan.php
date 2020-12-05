e1f3a0725d755af4da297d2bb3bdef1a3b44c24b
Michal Bukovský <michal.bukovsky@zasilkovna.cz>
2020-12-05 13:28:58 +0100

    day 3 part 2

--- days/03/Toboggan.php
+++ days/03/Toboggan.php
@@ -2,29 +2,47 @@
 
 class Toboggan extends Runner
 {
+    private const INCREMENTS_2 = [
+        ['x' => 1, 'y' => 1],
+        ['x' => 3, 'y' => 1],
+        ['x' => 5, 'y' => 1],
+        ['x' => 7, 'y' => 1],
+        ['x' => 1, 'y' => 2],
+    ];
 
     protected function runPart1(): string
     {
         $data = $this->getData(__DIR__);
-        $dataCount = count($data);
-        $hitTrees = 0;
 
-        // top left is 0,0
-        $x = 3;
-        for ($y = 1; $y < $dataCount; $y++) {
-            if ($this->isTreeHit($x, $y, $data)) {
-                $hitTrees++;
-            }
-            $x += 3;
-        }
-
-        return $hitTrees;
+        return $this->getTreesHit($data, 3, 1);
     }
 
     protected function runPart2(): string
     {
-        // TODO: Implement runPart2() method.
-        return '';
+        $data = $this->getData(__DIR__);
+
+        $treesHitMultiplied = 1;
+        foreach (self::INCREMENTS_2 as $increments) {
+            $treesHitMultiplied *= $this->getTreesHit($data, $increments['x'], $increments['y']);
+        }
+        return $treesHitMultiplied;
+    }
+
+    protected function getTreesHit(array $data, int $xIncrement, int $yIncrement): int
+    {
+        $dataCount = count($data);
+        $hitTrees = 0;
+
+        // top left is 0,0
+        $x = $xIncrement;
+        for ($y = $yIncrement; $y < $dataCount; $y += $yIncrement) {
+            if ($this->isTreeHit($x, $y, $data)) {
+                $hitTrees++;
+            }
+            $x += $xIncrement;
+        }
+
+        return $hitTrees;
     }
 
     protected function isTreeHit(int $x, int $y, array $data): bool
