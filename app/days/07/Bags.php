cb02bbaedbcd7525eaa4404de714871f8dfda493
Michal Bukovský <michal.bukovsky@zasilkovna.cz>
2020-12-07 08:24:51 +0100

    day 7 part 1

---
+++ app/days/07/Bags.php
@@ -0,0 +1,86 @@
+<?php
+
+class Bags extends Runner
+{
+    protected const NO_BAGS = 'no other bags';
+    protected const GOLD_BAG = 'shiny gold';
+
+    /**
+     * @var array
+     * Bag contents in this form:
+     * [
+     *   color => null,
+     *   color => [
+     *     color => 1,
+     *     color => 2,
+     *   ],
+     * ]
+     */
+    protected $bags;
+
+    public function __construct()
+    {
+        // wavy bronze bags contain 5 striped gold bags, 5 light tomato bags.
+        $this->initBags();
+    }
+
+    protected function runPart1(): string
+    {
+        $canContainGold = [];
+
+        foreach ($this->bags as $name => $contents) {
+            if ($this->canContainGoldBag($name)) {
+                $canContainGold[$name] = true;
+            }
+        }
+
+        return count($canContainGold);
+    }
+
+    protected function runPart2(): string
+    {
+        // TODO: Implement runPart2() method.
+    }
+
+    protected function initBags(): void
+    {
+        $data = $this->getData(__DIR__);
+
+        foreach ($data as $item) {
+            preg_match('~^(.+) bags contain (.+)\.$~', $item, $m);
+            [1 => $name, 2 => $contents] = $m;
+
+            if ($contents === self::NO_BAGS) {
+                $this->bags[$name] = null;
+                continue;
+            }
+
+            $bagContents = [];
+            $this->bags[$name] = preg_match_all('~(\d) ([a-z ]+) bags?(?:, )?~', $contents, $m, PREG_SET_ORDER);
+            foreach ($m as $bagMatch) {
+                $bagContents[$bagMatch[2]] = $bagMatch[1];
+            }
+            $this->bags[$name] = $bagContents;
+        }
+    }
+
+    protected function canContainGoldBag(string $name): bool
+    {
+        $contents = $this->bags[$name];
+        if ($contents === null) {
+            return false;
+        }
+        if (array_key_exists(self::GOLD_BAG, $contents)) {
+            return true;
+        }
+
+        foreach ($contents as $bagInsideName => $count) {
+            if ($this->canContainGoldBag($bagInsideName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
